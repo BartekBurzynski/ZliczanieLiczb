21cb620ffef6df164c42d9144c762fb9619e960f
BartekBurzynski <bartlomiej.burzynski@wp.pl>
2019-08-21 18:23:01 +0200

    Zliczanie wystapień liczb.

--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,34 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
 
-        List<Integer> num = new ArrayList<>();
-        num = downloadNumbers("text.txt");
-        frequencyCount (num);
-
+        List<Integer> num = null;
+        try {
+            num = downloadNumbers("text.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("nie ma takiego pliku");
         }
+        frequencyCount(num);
+
+    }
 
     private static void frequencyCount(List<Integer> numbers) {
         TreeSet<Integer> frequencyNumbers = new TreeSet<>(numbers);
         for (Integer number : frequencyNumbers) {
-            System.out.println(number + " Liczba wystąpień " + Collections.frequency(numbers,number));
+            System.out.println(number + " Liczba wystąpień " + Collections.frequency(numbers, number));
         }
     }
 
-    private static List<Integer> downloadNumbers(String s) throws FileNotFoundException {
+    private static List<Integer> downloadNumbers(String usedFile) throws FileNotFoundException {
         File file = new File("text.txt");
         Scanner input = new Scanner(file);
         List<Integer> fileNumbers = new ArrayList<>();
 
-        do {
-            if (input.hasNextInt()){
-                int fileNumber = input.nextInt();
-                fileNumbers.add(fileNumber);
-            }
-            } while (input.hasNextInt());
-
-        
+        while (input.hasNextInt()) {
+            int fileNumber = input.nextInt();
+            fileNumbers.add(fileNumber);
+        }
         return fileNumbers;
     }
 
